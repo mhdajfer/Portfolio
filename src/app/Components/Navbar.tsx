133148632c1544c0eb3ccd4e293b2e21c8c346b7
muhammed <ajferaju9961@gmail.com>
2025-06-08 08:04:33 +0530

    chore : remove pending footer

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     <div className="fixed w-full flex justify-evenly items-center  text-white text-3xl top-0">
       <h2>MA</h2>
       <ul className="flex gap-10 text-sm">
-        <li className="hover:underline cursor-pointer">Contact</li>
-        <li className="hover:underline cursor-pointer">Projects</li>
-        <li className="hover:underline cursor-pointer">Writes</li>
+        <li className="hover:underline cursor-pointer"></li>
+        <li className="hover:underline cursor-pointer"></li>
+        <li className="hover:underline cursor-pointer"></li>
       </ul>
     </div>
   );
